54d0443ef4a60ead27254b8adfefb594f50d5ff8
kavya-26331 <kavyasrimarri@gmail.com>
2026-09-18 21:32:53 +0530

    Update App.jsx

--- Walmart-Sales-Forecasting/frontend/dashboard/src/App.jsx
+++ Walmart-Sales-Forecasting/frontend/dashboard/src/App.jsx
@@ -307,7 +307,7 @@ function App() {
 
       const response =
         await fetch(
-          "http://127.0.0.1:8000/api/predict",
+          ""https://walmart-sales-forecasting-kzgb.onrender.com/api/predict",",
           {
             method: "POST",
 
